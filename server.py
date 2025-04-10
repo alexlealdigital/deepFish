22eb2624303e0e57878588fc12fcd9530255901a
Alex Leal <51205681+alexlealdigital@users.noreply.github.com>
2025-04-10 11:03:52 -0300

    Update server.py

--- server.py
+++ server.py
@@ -16,5 +16,6 @@ def obter_jogadas():
     global jogadas
     return jsonify({"jogadas": jogadas})
 
+# Adicione esta linha para o Gunicorn
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
