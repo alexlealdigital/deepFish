37feae1e6b7516fbc316db511fffe4e72977ac26
Alex Leal <51205681+alexlealdigital@users.noreply.github.com>
2025-04-10 10:58:24 -0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Variável global para armazenar as jogadas
+jogadas = 0
+
+@app.route('/incrementar_jogadas', methods=['POST'])
+def incrementar_jogadas():
+    global jogadas
+    jogadas += 1
+    return jsonify({"jogadas": jogadas})
+
+@app.route('/obter_jogadas', methods=['GET'])
+def obter_jogadas():
+    global jogadas
+    return jsonify({"jogadas": jogadas})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
